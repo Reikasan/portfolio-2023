35820ac7e002b2e5897fa441c2c89b3368a6a01f
Reika Akuzawa <l06981@googlemail.com>
2023-12-30 00:05:08 +0100

    Update about me data

--- src/data/about-me-data.ts
+++ src/data/about-me-data.ts
@@ -1,12 +1,54 @@
+import travelPhoto from '../assets/images/travel.jpg';
+import japanPhoto from '../assets/images/japan.jpg';
+import ikebanaPhoto from '../assets/images/ikebana.jpg';
+import berlinPhoto from '../assets/images/berlin.jpg';
+import weightTrainingPhoto from '../assets/images/weight-training.jpg';
+import diamondPhoto from '../assets/images/diamond.jpg';
+
 export const aboutMeData = [
     {
-        imageSrc: '',
-        title: 'Photography',
-        description: 'I love taking photos of nature and landscapes.',
+        imageSrc: travelPhoto,
+        title: 'Travel',
+        description: 'Sometimes I need some new experience. Go to local supermarket is one of my favorite things to do!',
     },
     {
-        imageSrc: '',
+        imageSrc: japanPhoto,
         title: 'Japan',
         description: 'My root country, Japan. It always has a special place in my heart.',
-    }
+    },
+    {
+        imageSrc: ikebanaPhoto,
+        title: 'Ikebana',
+        description: 'Ikebana is art of japanese flower arrangement. My mother is a teacher of ikebana, so I have been learning it since I was a child.',
+    },
+    {
+        imageSrc: berlinPhoto,
+        title: 'Berlin',
+        description: 'I love this city because it is so international and has a lot of opportunities.',
+    },
+    {
+        imageSrc: weightTrainingPhoto,
+        title: 'Weight training',
+        description: 'I love weight training, especially in the morning time. My record is 65kg hip-thrust.',
+    },
+    {
+        imageSrc: diamondPhoto,
+        title: 'Diamond',
+        description: 'My first Job was a sorter of small diamonds. Each diamond has different color of shining. Since then I have been interested in the beauty of light.',
+    },
+    {
+        imageSrc: japanPhoto,
+        title: 'Japan',
+        description: 'My root country, Japan. It always has a special place in my heart.',
+    },
+    {
+        imageSrc: japanPhoto,
+        title: 'Japan',
+        description: 'My root country, Japan. It always has a special place in my heart.',
+    },
+    {
+        imageSrc: japanPhoto,
+        title: 'Japan',
+        description: 'My root country, Japan. It always has a special place in my heart.',
+    },
 ]
