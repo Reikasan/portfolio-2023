b5156aa71232112da95cfa2b4051987453c29bf8
Reika Akuzawa <l06981@googlemail.com>
2024-01-01 18:35:03 +0100

    Update about me data

--- src/data/about-me-data.ts
+++ src/data/about-me-data.ts
@@ -1,54 +1,74 @@
 import travelPhoto from '../assets/images/travel.jpg';
-import japanPhoto from '../assets/images/japan.jpg';
+import japanPhoto from '../assets/images/japan.jpeg';
 import ikebanaPhoto from '../assets/images/ikebana.jpg';
 import berlinPhoto from '../assets/images/berlin.jpg';
 import weightTrainingPhoto from '../assets/images/weight-training.jpg';
 import diamondPhoto from '../assets/images/diamond.jpg';
+import cookingPhoto from '../assets/images/cooking.jpg';
+import photographyPhoto from '../assets/images/photography.jpeg';
+import pianoPhoto from '../assets/images/piano.jpg';
+import flightPhoto from '../assets/images/flight.jpeg';
+
 
 export const aboutMeData = [
-    {
+    {   
+        id: 1,
         imageSrc: travelPhoto,
         title: 'Travel',
         description: 'Sometimes I need some new experience. Go to local supermarket is one of my favorite things to do!',
     },
     {
-        imageSrc: japanPhoto,
-        title: 'Japan',
-        description: 'My root country, Japan. It always has a special place in my heart.',
+        id: 2,
+        imageSrc: cookingPhoto,
+        title: 'Cooking',
+        description: 'Cooking is a good practice for multi-tasking. I cook Japanese food for necessity, but I also like to cook sweets and bread.',
     },
     {
+        id: 3,
         imageSrc: ikebanaPhoto,
         title: 'Ikebana',
         description: 'Ikebana is art of japanese flower arrangement. My mother is a teacher of ikebana, so I have been learning it since I was a child.',
     },
     {
+        id: 4,
         imageSrc: berlinPhoto,
         title: 'Berlin',
         description: 'I love this city because it is so international and has a lot of opportunities.',
     },
     {
+        id: 5,
         imageSrc: weightTrainingPhoto,
-        title: 'Weight training',
-        description: 'I love weight training, especially in the morning time. My record is 65kg hip-thrust.',
+        title: 'Training',
+        description: 'Every morning I do training. One day, Yoga, other day with over 50 kg weight. Morning Exercise makes me feel good for the whole day!',
     },
     {
+        id: 6,
         imageSrc: diamondPhoto,
         title: 'Diamond',
-        description: 'My first Job was a sorter of small diamonds. Each diamond has different color of shining. Since then I have been interested in the beauty of light.',
+        description: 'My first Job was a sorter of small diamonds. Each diamond has different color of shining. Since then I have been interested in the beauty of natural creations.',
     },
     {
-        imageSrc: japanPhoto,
-        title: 'Japan',
-        description: 'My root country, Japan. It always has a special place in my heart.',
+        id: 7,
+        imageSrc: photographyPhoto,
+        title: 'Photography',
+        description: 'One of my hobby is photography. I like to take pictures of daily life.',
     },
     {
-        imageSrc: japanPhoto,
-        title: 'Japan',
-        description: 'My root country, Japan. It always has a special place in my heart.',
+        id: 8,
+        imageSrc: pianoPhoto,
+        title: 'Piano',
+        description: '2023 I started to learn piano again. Every day 10 minutes is not much but its enough to see my progress!',
     },
     {
+        id: 9,
         imageSrc: japanPhoto,
         title: 'Japan',
-        description: 'My root country, Japan. It always has a special place in my heart.',
+        description: 'My root country, Japan. It always has a special place in my heart. My diligence and thoughtfulness comes from my experience working in Japan.',
+    },
+    {
+        id: 10,
+        imageSrc: flightPhoto,
+        title: 'Flight',
+        description: "I'm always excited to see the world from the sky. ",
     },
 ]
