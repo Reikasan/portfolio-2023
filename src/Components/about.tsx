eb7cf90eddb4b9cd84a5e3b5531ced58807d59fb
Reika Akuzawa <l06981@googlemail.com>
2024-01-02 23:25:51 +0100

    Add about text

--- src/Components/about.tsx
+++ src/Components/about.tsx
@@ -1,30 +1,61 @@
 import RunningMessage from "./running-message";
-import { Container } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHand } from "@fortawesome/free-regular-svg-icons";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 import PhotosAboutMe from "./photos-about-me";
+import lightsImg from "../assets/images/lights.jpg";
+
+const aboutImageStyle = {
+    width: '40vw', 
+    height: '220px', 
+    position: 'absolute' as 'absolute', 
+    right: 0, 
+    top: 0, 
+    opacity: '0.4', 
+    overflow: 'hidden',
+    zIndex: -1,
+}
+
+const aboutBgSqStyle = {
+    width: '150px', 
+    height: '150px', 
+    position: 'absolute' as 'absolute', 
+    right: '33vw', 
+    bottom: 0, 
+    opacity: '0.4', 
+    backgroundColor: '#4abea0',
+    zIndex: -1, 
+}
 
 const about = () => {
     return (
         <section id="about" className="about">
             <RunningMessage message="about" direction="from-left" />
-            <Container maxWidth="md"  
-                    sx={{
-                        padding: '5vh 0 5vh 0',
-                    }}>
-                    <div className="about__text">
-                        <h2>Hello, I am Reika
-                        <FontAwesomeIcon icon={(faHand)} />
-                        </h2>
+            <Container maxWidth="md" sx={{padding: '10vh 0'}}>
+                <Box sx={{ margin: '0 0 10vh', height: '300px'}}>
+                    <Box className="about__text" sx={{width: '80%'}}>
+                        <Box sx={{marginBottom: '3rem'}}>
+                            <FontAwesomeIcon icon={faQuoteLeft} className="quote-icon"/>
+                            <Typography variant="h3" component="h2" sx={{width: '80%', marginLeft: '1rem', fontWeight: '100'}}>
+                                How can I change the life a bit better?
+                            </Typography>
+                        </Box>
                         <p>
-                            Since 2018 I started learning programming. I am a web developer based in Berlin.
-                            I have experience in building web applications with React and Node.js.
-                            I am currently looking for a job as a Frontend Developer.
+                            I always think about it.<br/>
+                            The difference, what I can make could be a small , but I believe one day it can make someone happy.
                         </p>
-                    </div>
-                    <PhotosAboutMe />
-                </Container>
-            </section>
+                    </Box>
+                    
+                </Box>
+                <PhotosAboutMe />
+            </Container>
+            <Box sx={{height: '300px', position: 'absolute' as 'absolute', top: '200px', right: 0}}>
+                <Box className="about__image" sx={aboutImageStyle}>
+                    <img src={lightsImg} alt="lights" />
+                </Box>
+                <Box sx={aboutBgSqStyle}></Box>
+            </Box>
+        </section>
     );
 }
 
