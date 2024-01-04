f5ecfe0fde93f955ee0e697cc7fd81dcf3d857e8
Reika Akuzawa <l06981@googlemail.com>
2024-01-04 11:42:23 +0100

    Add ogp settings with react-helmet-async

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,38 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const ogpData = {
+  title: 'Reika Akuzawa Portfolio Site',
+  description: 'Reika Akuzawa is a Junior Web developer based in Berlin.',
+  image: '%PUBLIC_URL%/src/assets/images/ogp-image.jpg',
+  url: '%PUBLIC_URL%',
+  type: 'website',
+  facebookAppId: process.env.REACT_APP_FACEBOOK_APP_ID,
+};
+
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <HelmetProvider>
+      <Helmet>
+        <meta property="og:title" content={ogpData.title} />
+        <meta property="og:description" content={ogpData.description} />
+        <meta property="og:image" content={ogpData.image} />
+        <meta property="og:type" content={ogpData.type} />
+        <meta property="og:url" content={ogpData.url} />
+
+        <meta name="description" content={ogpData.description} />
+        <title>{ogpData.title}</title>
+      </Helmet>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
 
