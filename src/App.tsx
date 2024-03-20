8d1aa963ca2851da272becb47bab3a6e8729dde2
Reika Akuzawa <l06981@googlemail.com>
2024-03-20 09:44:32 +0100

    Add react-cookiebot package and configure Cookiebot ID

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,18 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Cookiebot from 'react-cookiebot';
 import TopPage from './pages/top-page/TopPage';
 
 function App() {
+  const cookieBotId = process.env.REACT_APP_COOKIEBOT_ID;
+
+  if(!cookieBotId) {
+    throw new Error('Cookiebot ID is not set');
+  }
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Cookiebot domainGroupId={cookieBotId} />
       <Routes>
         <Route path="/" element={<TopPage/>} />
       </Routes>
